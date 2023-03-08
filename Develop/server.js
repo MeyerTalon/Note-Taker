ee082e1ef41fc5932b5f0de66100b3e03ae53cbe
Talon Meyer <talonmeyer12@gmail.com>
2023-03-07 22:46:00 -0800

    Added api.js file and connected it to server.js file

--- Develop/server.js
+++ Develop/server.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const path = require('path');
 const fs = require('fs');
+const api = require('./routes/api.js');
 
 const PORT = process.env.PORT || 3001;
 
@@ -8,6 +9,7 @@ const app = express();
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
+app.use('/api', api);
 
 app.use(express.static('public'));
 
